6a144e41da5222154e73784af4e181e132984162
guts91 <yoon1134@gmail.com>
2026-08-24 00:11:51 +0900

    feat: Article 생성 기능 구현

--- ArticleService.js
+++ ArticleService.js
@@ -29,3 +29,27 @@ export function getArticle(articleId) {
       console.error(error.message);
     });
 }
+
+export function createArticle(title, content, image) {
+  return fetch(`${BASE_URL}/articles`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title,
+      content,
+      image,
+    }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP Error: ${response.status}`);
+      }
+
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+}
--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-import { getArticleList, getArticle } from "./ArticleService.js";
+import { getArticleList, getArticle, createArticle } from "./ArticleService.js";
 
-getArticleList(1, 10, "").then((data) => {
-  console.log(data);
-});
+// getArticleList(1, 10, "").then((data) => {
+//   console.log(data);
+// });
 
-getArticle(6908).then((data) => {
+// getArticle(6908).then((data) => {
+//   console.log(data);
+// });
+
+createArticle(
+  "스프린트 미션3 테스트 게시글",
+  "createArticle 테스트입니다.",
+  "https://picsum.photos/300",
+).then((data) => {
   console.log(data);
 });
