d1dd69ff3b55d2aed301e58b81e32535ccb7034d
Limug-g <dmlrbs94@gmail.com>
2026-08-29 16:12:51 +0900

    Add: prisma.js 파일 생성, prisma 인스턴스 생성

---
+++ src/db/prisma.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '#generated/prisma/client.ts';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { config } from '../config/config.js';
+
+const adapter = new PrismaPg({
+  connectionString: config.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({ adapter });
